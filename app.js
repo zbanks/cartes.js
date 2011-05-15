4844161cd168dd07bf51a8be396cf085cdeac077
Zach Banks <zach@zbanks.net>
2011-05-15 00:21:15 -0400

    Authentication

--- app.js
+++ app.js
@@ -5,7 +5,10 @@
 
 var express = require('express');
 var connect = require('connect');
+var auth = require('connect-auth');
 var RedisStore = require('connect-redis');
+var redis = require("redis").createClient();
+var authStrategy = require('./authStrategy');
 
 var app = module.exports = express.createServer();
 
@@ -17,6 +20,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: "ragoflago", store: new RedisStore }));
+  app.use(auth(authStrategy()));
+
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
@@ -34,9 +39,24 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+    req.session.count = (req.session.count || 0) + 1;
+    res.render("index", {
+        title: "Express " + req.session.count,
+    });
+});
+
+app.get('/test', function(req, res){
+    req.authenticate(['dbAuth'], function(error, authenticated){
+        if(authenticated){
+            res.render('index', {
+                title: 'Yay, ' + req.session.user + '!'
+            });
+        }else{
+            res.render('index', {
+                title: 'awww...'
+            });
+        }
+    });
 });
 
 // Only listen on $ node app.js
