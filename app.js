866eaf9ef6fde5dcacf2c4c709ea2a3ac98921a7
Zach Banks <zach@zbanks.net>
2011-05-23 15:32:03 -0400

    Github repo

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var express = require('express');
 var connect = require('connect');
 var auth = require('connect-auth');
 var RedisStore = require('connect-redis');
-var redis = require("redis").createClient();
 var authStrategy = require('./authStrategy');
+var db = require("./redisdb");
+var Sandbox = require("sandbox");
 
 var app = module.exports = express.createServer();
+var redis = db.redis;
 
 // Configuration
 
@@ -20,11 +22,11 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({ secret: "ragoflago", store: new RedisStore }));
+  app.dynamicHelpers({ messages: require('express-messages') });
   app.use(auth(authStrategy()));
-
   app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -36,6 +38,18 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+// Middleware
+
+function requireAuth(req, res, next){
+    req.authenticate(['dbAuth'], function(error, authenticated){
+        if(authenticated){
+            next();
+        }else{
+            res.redirect('/auth/form_callback');
+        }
+    });
+}
+
 // Routes
 
 app.get('/', function(req, res){
@@ -45,17 +59,84 @@ app.get('/', function(req, res){
     });
 });
 
-app.get('/test', function(req, res){
-    req.authenticate(['dbAuth'], function(error, authenticated){
-        if(authenticated){
-            res.render('index', {
-                title: 'Yay, ' + req.session.user + '!'
-            });
+app.get('/test', requireAuth, function(req, res){
+    res.render('index', {
+        title: 'Yay, ' + req.session.user + '!'
+    });
+});
+
+
+
+app.post("/signup", function(req, res, next){
+    var make_hash = function(password){
+        var salt = (+new Date()).toString();
+        return salt + "$" + crypto.createHash("sha512").update(salt+password).digest("hex");
+    }
+    if(!req.body.user || !req.body.password || !req.body.verify){
+        return next();
+    }
+    var username = db.sanitize_user(req.body.user);
+    if(username != req.body.user){
+        req.flash("error", "Invalid username. Letters, numbers, and underscores only");
+    }
+    if(req.body.password != req.body.verify){
+        req.flash("error", "Passwords do not match");
+        return next();
+    }
+    redis.get("username:" + req.body.user + ":uid", function(err, v){
+        if(err) throw err;
+        if(v){
+            req.flash("error", "Username taken");
+            return next();
         }else{
-            res.render('index', {
-                title: 'awww...'
+            redis.incr("global:nextUserId", function(err, uid){
+                if(err) throw err;
+                var pwhash = db.make_hash(req.body.password);
+                
+                redis.mset(
+                    db.build("uid", uid, "username"), req.body.user, 
+                    db.build("uid", uid, "password"), pwhash,
+                    db.build("username", req.body.user, "uid"), uid,
+                    function(err){ 
+                        if(err) throw err; 
+                        req.flash("info", "Signup sucessful");
+                        res.render("login", {
+                            title: "Login",
+                            redirectUrl: "/auth/form_callback?redirect_url=/"
+                        });
+                    }
+                );
             });
         }
+    });
+});
+
+app.all("/signup", function(req, res){
+    res.render("signup", {
+        title: "Signup",
+        redirectUrl: "/signup"
+    });
+});
+
+app.get("/code", requireAuth, function(req, res){
+    req.authenticate(['dbAuth
+});
+
+app.post("/exec", requireAuth, function(req, res, next){
+    if(req.body.code){
+        s = new Sandbox();
+        s.run(req.body.code, function(output){
+            req.flash("info", output.result);
+            next();
+        });
+    }else{
+        next();
+    }
+});
+
+app.all("/exec", requireAuth, function(req, res){
+    res.render("exec", {
+        title: "Execute JS"
     });
 });
 
